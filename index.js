c11e8e9d8032ea987cb2e51d728800a9963cc08c
murunwas <cengyweb@gmail.com>
2017-10-02 21:42:40 +0200

    add html

--- index.js
+++ index.js
@@ -4,18 +4,15 @@ var express = require('express');
 var cors = require('cors')
 var app = express();
 var port = process.env.PORT || 4300;
-var ip = process.env.IP || 'http://localhost';
-
+var pass = process.env.pass;
+var user = "webcengy@gmail.com"
 
 
 
 
 var transporter = nodemailer.createTransport({
   service: 'gmail',
-  auth: {
-    user: 'webcengy@gmail.com',
-    pass: process.env.pass
-  }
+  auth: { user: user, pass: pass }
 });
 
 //Use
@@ -24,36 +21,53 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(cors())
 
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.send("Hello Cengy!");
 });
 
-app.get('/hello', function(req, res){
-   res.send("Hello World!");
+app.get('/hello', function (req, res) {
+  res.send("Hello World!");
 });
 
-app.post('/mail', function(req, res){
+app.post('/mail', function (req, res) {
 
   var mailOptions = {
-    from: req.body.from,
-    to:   req.body.to,
+    from: '"Our Diary" <' + req.body.from + '>',
+    to: req.body.to,
     subject: req.body.subject,
-    text: req.body.message
+    html:
+    `<div style="width:100%; padding: 6px;text-align: center">
+    <label style="color: #B4886B;
+    font-weight: bold;
+    display: block;">${req.body.from} sent a diary</label>
+    <hr>
+    <p>${req.body.message}</p>
+ 
+    <a href="https://shudie.murunwa.ga/" style="    background-color: #4CAF50; /* Green */
+        border: none;
+        color: white;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;width: 100%;">click to view</a>
+    </div>
+    
+    `
   };
-  
-  transporter.sendMail(mailOptions, function(error, info){
+
+  transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
       console.log(error);
-
-      res.json({status: false, message: error});
+      res.json({ status: false, message: error, data: mailOptions });
     } else {
       console.log('Email sent: ' + info.response);
-      res.json({status: true, message: info.response});
+      res.json({ status: true, message: info.response });
     }
   });
-   
+
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log('Our app is running on Port: ' + port);
 });
